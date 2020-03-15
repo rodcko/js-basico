0783aa3a09764b7e41311209024404568843e88e
rodcko2417 <rodcko@live.com>
2020-03-15 00:00:23 -0600

    Condicionales: If, Else, Else If

---
+++ if.js
@@ -0,0 +1,27 @@
+/*Ejemplo 1*/
+if (true) {
+    console.log("Hola");
+} else if () {
+    console.log("soy falso");
+}
+
+
+/*Ejemplo 2*/
+var edad = 18;
+
+if(edad === 18) {
+    console.log("Puedes votar, será tu 1ra votación");
+} else if (edad > 18) {
+    console.log("Puedes votar de nuevo")
+} else {
+    console.log("Aún no puedes votar")
+}
+
+/*Ejemplo 3 "if ternario"*/
+condition ? true : false;
+
+var numero = 1;
+
+var resultado = numero === 1 ? "Sí soy un uno" : "No, no soy uno";
+
+
