2e355a4110107c98aaca475e785d2d839fb75101
rodcko2417 <rodcko@live.com>
2020-03-15 18:27:59 -0600

    Objects: función constructora

--- Objects.js
+++ Objects.js
@@ -13,3 +13,15 @@ miAuto.marca;
 
 // Obtener un metodo
 miAuto.detalleDelAuto();
+
+// Función constructora
+function auto(marca, modelo, annio) {
+    this.marca = marca;
+    this.modelo = modelo; 
+    this.annio = annio;
+}
+
+// Crear una instancia de objeto a partir de la función constructora
+var autoNuevo = new auto("Tesla", "Model 3", 2020);
+var autoNuevo2 =  new auto("Tesla", "Model X", 2018);
+
